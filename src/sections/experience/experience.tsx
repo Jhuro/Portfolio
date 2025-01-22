34ca31db3bd5570e090b7bc50f285a66503ab527
jupbc <jupbcan100@hotmail.com>
2025-01-22 02:16:08 -0500

    Fix
    Binding data for items at experience item

--- src/sections/experience/experience.tsx
+++ src/sections/experience/experience.tsx
@@ -1,17 +1,27 @@
 import {useTranslations} from 'next-intl';
 import ExperienceCard from '@/components/experienceCard';
 
+interface ExperienceItem  {
+  image: string;
+  type: string;
+  startDate: string;
+  endDate: string;
+  title: string; 
+  subtitle: string;
+}
+
 export default function Experience() {
   const t = useTranslations('Experience');
+  const items: ExperienceItem[] = t.raw('items') as ExperienceItem[];
   return (
     <section id="experience" className="p-8 mx-auto">
         <h2 className="text-3xl font-semibold mb-6">
           {t('title')}
         </h2>
         <div className="space-y-4 items-center">
-            {t.raw('items').map((item, index) => (
-              <ExperienceCard key={index} {...item} />
-            ))}
+          {items.map((item, index) => (
+            <ExperienceCard key={index} {...item} />
+          ))}
         </div>
     </section>
   );
